feb1ac6f7120eb238a2583127381d1c1fec5bcbc
Ripparrroo123 <54618703+Ripparrroo123@users.noreply.github.com>
2020-10-14 14:03:33 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,9 +1,26 @@
+import djur
+import foodmanager
+import hygienmanager
+import healthmanager
+
+import sys
+
 class Menu:
 
     
-    def __init__(self, title, menu_items):
-        self.__menu_items=(menu_items)
+    def __init__(self, title, menu_items, food_items, hygene_Options, activities):
+        self.__menu_items = (menu_items)
+        self.__food_items = (food_items)
+        self.__hygene_Options = (hygene_Options)
+        self.__activities = (activities)
+        self.__settings = ['dark/light mode']
         self.__title=title
+
+        self.__darkMode = False
+
+        self.__foodmanager = foodmanager.FoodManager()
+        self.__hygienmanager = hygienmanager.HygienManager()
+        self.__healthmanager = healthmanager.HealthManager()
 
 
     def show_menu(self):
@@ -12,7 +29,59 @@ class Menu:
         for i, item in enumerate(self.__menu_items):
             print(f'{i+1}: {item}')
 
-main_menu = Menu('Huvudmeny', ['Status', 'Mat', 'Hygien', 'Aktiviteter', 'Inställningar', 'Avsluta'])
+    def printStatus(self):
+        print(djur.status())
+    
+    def show_Food(self):
+        for i, item in enumerate(self.__food_items):
+            print(f'{i+1}: {item}')
+        foodRequest = input().lower()
+        self.__foodmanager.eat(foodRequest)
+        Menu.printStatus()
+
+    def show_Hygene(self):
+        for i, item in enumerate(self.__hygene_Options):
+            print(f'{i+1}: {item}')
+        hygeneRequest = input().lower()
+        self.__hygienmanager.do(hygeneRequest)
+        Menu.printStatus()
+
+    def show_Activities(self):
+        for i, item in enumerate(self.__activities):
+            print(f'{i+1}: {item}')
+        activityRequest = input().lower()
+        self.__healthmanager.activity(activityRequest)
+        Menu.printStatus()
+
+    def show_Settings(self):
+        for i, item in enumerate(self.__settings):
+            print(f'{i+1}: {item}')
+        settingRequest = input().lower()
+        if settingRequest == 'dark/light mode' or settingRequest == 'darkmode' and self.__darkMode == False or settingRequest == 'lightmode' and self.__darkMode == True:
+           self.__darkMode = not self.__darkMode 
+
+
+
+
+main_menu = Menu('Huvudmeny', ['Status', 'Mat', 'Hygien', 'Aktiviteter', 'Inställningar', 'Avsluta'], ['candy', 'meat', 'water'], ['shower', 'hand sanitizer'], ['play', 'exercise'])
+
+djur = djur.Djur('Jure')
 
 main_menu.show_menu()
 
+request = input().lower()
+
+if request == 'status':
+    Menu.printStatus()
+elif request == 'mat':
+    Menu.show_Food()
+elif request == 'hygien':
+    Menu.show_Hygene()
+elif request == 'aktiviteter':
+    Menu.showActivities()
+elif request == 'inställningar':
+    Menu.show_Settings
+elif request == 'avsluta':
+    sys.exit(0)
+else:
+    print('I don\'t know what that means...')
