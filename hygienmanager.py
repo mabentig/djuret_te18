9e514cba627d2e9faeb5c53f4ae23c06682eb5b6
69Napalm69 <mattsson.gabriel@gmail.com>
2020-10-20 13:17:11 +0200

    reformating_yes
    vi_flytta_koden_så_den_såg_bättre_ut

--- hygienmanager.py
+++ hygienmanager.py
@@ -1,25 +1,27 @@
 import random
 class HygienManager:
 
+
     def __init__(self):
         self.hygiene = 40
         self.__pills_taken = 0
+    
 
     def update(self, elapsed_time):
         """
-        För varje sekund som gått har Gurbert antingen blivit smuttsigare eller inte.
+        För varje sekund som gått har Gurkbert antingen blivit smutsigare eller inte.
         """
         for x in range(elapsed_time):
             
             self.hygiene -= random.shuffle([0,1])[0]
-            
         
         self.__pills_taken = max(self.__pills_taken-elapsed_time/10, 0)
                 
         if self.__pills_taken > 10:
-            self.hygiene = -65
+            self.hygiene = -69
             
         return 0
+
 
     def shower(self):
         """
@@ -29,6 +31,7 @@ class HygienManager:
         
         return 0
 
+
     def hand_sanitiser(self):
         """
         Detta är handsprit så det är snabbare att skriva men get ett mindre värde.
@@ -37,8 +40,7 @@ class HygienManager:
         
         return 0
     
-    
-    
+
     def take_pills(self):
         """
         Detta gör så du tar ett piller, vilket sänker chansen till disease och ökar hygiene med 40
