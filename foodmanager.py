5542cadd6e5ded3e49152f097926df8ba26346a4
Z4R1N <TheOnlyGod.ZARIN@gmail.com>
2020-10-21 14:23:15 +0200

    Update foodmanager.py

--- foodmanager.py
+++ foodmanager.py
@@ -7,71 +7,60 @@ class FoodManager:
         self.thirst = random.randint(30,60)
         self.happiness = random.randint(30,60)
 
-        """ ToDo
-        self.later = [
 
-        ]
-        """
-
-        self.inventorty = {
-            "GoldenApple": {
-                "Level":        10,
-                "Unlocked":     False,
-                "Amount":       0,
-
-                "Hunger":       -10,
-                "Thirst":       -10,
-                "Happiness":    +25
-            },
-
-            "Water": {
+        self.inventory = [
+            {
+                "Name":         "Water",
                 "Level":        1,
-                "Unlocked":     False,
-                "Amount":       0,
 
                 "Hunger":       -2,
                 "Thirst":       +10,
                 "Happiness":    0 
             },
-
-            "Bannana": {
+            {
+                "Name":         "Bannana",
                 "Level":        1,
-                "Unlocked":     False,
-                "Amount":       0,
 
                 "Hunger":       +10,
                 "Thirst":       -2,
                 "Happiness":    0
             },
-
-            "Tequila": {
-                "Level":        18,
-                "Unlocked":     False,
-                "Amount":       0,
-
-                "Hunger":       0,
-                "Thirst":       0,
-                "Happiness":    0 
-            },
-
-            "Joj kotfärssås": {
+            {
+                "Name":         "JojKotfärssås",
                 "Level":        5,
-                "Unlocked":     False,
-                "Amount":       0,
 
                 "Hunger":       +10,
                 "Thirst":       +5,
                 "Happiness":    -20
             },
-        }
+            {
+                "Name":         "GoldenApple",
+                "Level":        10,
 
+                "Hunger":       -10,
+                "Thirst":       -10,
+                "Happiness":    +25
+            },
+            {
+                "Name":         "Tequila",
+                "Level":        18,
+
+                "Hunger":       0,
+                "Thirst":       0,
+                "Happiness":    0 
+            }
+        ]
     def update(self, elapsed_time):
         return 0
 
-    def use(self, item):
-        if self.inventorty[item] and self.inventorty[item].Amount > 0:
-            self.inventorty[item].Unlocked = True
+    def backpack(self):
+        for i, v in enumerate(self.inventory):
+            print(i, ": ", v["Name"])
 
-            self.hunger += self.inventorty[item].Hunger
-            self.thirst += self.inventorty[item].Thirst
-            self.happiness += self.inventorty[item].Happiness
+    def use(self, item):
+        if self.inventory[item] and self.inventory[item].Amount > 0:
+            self.inventory[item].Unlocked = True
+
+            self.hunger += self.inventory[item].Hunger
+            self.thirst += self.inventory[item].Thirst
+            self.happiness += self.inventory[item].Happiness
